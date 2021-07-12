ddec39f9591a05ef6072d0bbdb9709f231e8f41f
fish895623 <dan990429@gmail.com>
2021-07-12 13:25:22 +0900

    Fix indents and settings

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -20,52 +20,53 @@ import java.util.Map;
 
 public class MainActivity extends AppCompatActivity {
 
-    private static final String TAG = "MAIN";
-    private TextView tv;
-    private EditText sentenceBox;
-    private Button btnSend;
-    private RequestQueue queue;
+  private static final String TAG = "MAIN";
+  private TextView tv;
+  private EditText sentenceBox;
+  private Button btnSend;
+  private RequestQueue queue;
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        tv = findViewById(R.id.tvMain);
-        sentenceBox = findViewById(R.id.sentence);
-        btnSend = findViewById(R.id.btnSend);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    tv = findViewById(R.id.tvMain);
+    sentenceBox = findViewById(R.id.sentence);
+    btnSend = findViewById(R.id.btnSend);
 
-        queue = Volley.newRequestQueue(this);
-        String url = "http://oreopie.ipdisk.co.kr:5000/transformer/post";
+    queue = Volley.newRequestQueue(this);
+    String url = "http://oreopie.ipdisk.co.kr:5000/transformer/post";
 
-        final StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
-            @Override
-            public void onResponse(String response) {
-                tv.setText(response);
-            }
-        }, error -> {}) {
-            @Override
-            protected Map<String, String> getParams() throws AuthFailureError {
-                Map<String, String> params = new HashMap<String, String>();
-                params.put("sentence", sentenceBox.getText().toString());
-                return params;
-            }
-        };
+    final StringRequest stringRequest = new StringRequest(Request.Method.POST, url, new Response.Listener<String>() {
+      @Override
+      public void onResponse(String response) {
+        tv.setText(response);
+      }
+    }, error -> {
+    }) {
+      @Override
+      protected Map<String, String> getParams() throws AuthFailureError {
+        Map<String, String> params = new HashMap<String, String>();
+        params.put("sentence", sentenceBox.getText().toString());
+        return params;
+      }
+    };
 
-        stringRequest.setTag(TAG);
+    stringRequest.setTag(TAG);
 
-        btnSend.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                queue.add(stringRequest);
-            }
-        });
+    btnSend.setOnClickListener(new View.OnClickListener() {
+      @Override
+      public void onClick(View v) {
+        queue.add(stringRequest);
+      }
+    });
+  }
+
+  @Override
+  protected void onStop() {
+    super.onStop();
+    if (queue != null) {
+      queue.cancelAll(TAG);
     }
-
-    @Override
-    protected void onStop() {
-        super.onStop();
-        if (queue != null) {
-            queue.cancelAll(TAG);
-        }
-    }
+  }
 }
